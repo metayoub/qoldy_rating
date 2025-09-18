13ea805b38fd0ebaacf5bbd8fc2c177cb1797f31
Ayoub METWALLI <ayoub.metwalli@4d.com>
2025-09-18 15:47:20 +0100

    Migrate the cli to the latest version

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
-import { PluginOption, defineConfig } from 'vite';
+import { PluginOption, defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import importMetaUrlPlugin from '@ws-ui/vite-plugins/dist/esbuild-plugin-import-meta-url';
+import monacoEditorPlugin from '@ws-ui/vite-plugins/dist/vite-plugin-monaco-editor';
+import { federation } from '@module-federation/vite';
+import { initProxy } from './proxy.config';
+import { dependencies as deps, app_id } from './package.json';
 
-import proxy from './proxy.config';
+const exposes = {
+  './components': './src/components/index.tsx',
+};
 
-const port = process.env.PORT || 5001;
-const host = process.env.HOST || '0.0.0.0';
+const isDevEnv = process.env.NODE_ENV === 'development';
 
 const redirect = (opts: { from: string; to: string }): PluginOption => {
   return {
@@ -24,21 +30,91 @@ const redirect = (opts: { from: string; to: string }): PluginOption => {
   };
 };
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    redirect({
-      from: '/studio/',
-      to: '/',
+const getBuildPlugins = () => {
+  if (isDevEnv) {
+    return [monacoEditorPlugin()];
+  }
+
+  return [
+    federation({
+      name: app_id,
+      filename: 'components.js',
+      exposes,
+      library: { type: 'var' },
+      shared: {
+        ...['react', 'react-dom', 'react/jsx-runtime'].reduce(
+          (acc, key) => ({
+            ...acc,
+            [key]: {
+              requiredVersion: deps[key],
+              singleton: true,
+              eager: true,
+            },
+          }),
+          {},
+        ),
+        ...[
+          // @ws-ui
+          '@ws-ui/webform-editor',
+          '@ws-ui/craftjs-core',
+          '@ws-ui/craftjs-layers',
+          '@ws-ui/craftjs-utils',
+          '@ws-ui/shared',
+        ].reduce(
+          (acc, key) => ({
+            ...acc,
+            [key]: {
+              singleton: true,
+              eager: true,
+            },
+          }),
+          {},
+        ),
+      },
     }),
-  ],
-  define: {
-    'process.env': {},
-  },
-  server: {
-    host,
-    proxy,
-    port: +port,
-  },
+  ];
+};
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode = 'local' }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  const port = env.PORT || 5001;
+  const host = env.HOST || '0.0.0.0';
+
+  const proxy = initProxy(env);
+
+  return {
+    plugins: [
+      react(),
+      redirect({
+        from: '/studio/',
+        to: '/',
+      }),
+      ...getBuildPlugins(),
+    ],
+    define: {
+      'process.env': {},
+    },
+    optimizeDeps: {
+      esbuildOptions: {
+        plugins: isDevEnv ? [importMetaUrlPlugin] : [],
+      },
+    },
+    server: {
+      host,
+      proxy,
+      port: +port,
+    },
+    build: {
+      rollupOptions: {
+        external: ['@ws-ui/code-editor'],
+        output: {},
+      },
+      modulePreload: false,
+      target: 'esnext',
+      minify: false,
+      cssCodeSplit: false,
+    },
+  };
 });
